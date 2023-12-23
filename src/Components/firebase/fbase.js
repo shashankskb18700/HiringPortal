606571dc23eb68ad4006b3e050835868b03460b4
shashank <shashankskb18700@gmail.com>
2023-12-23 22:10:28 +0530

    inital commit

--- src/Components/firebase/fbase.js
+++ src/Components/firebase/fbase.js
@@ -13,13 +13,13 @@ import * as db from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5EAJbTq45XTHWunol_9Z9wZv6OQZh5io",
-  authDomain: "hiringportal-daf0a.firebaseapp.com",
-  projectId: "hiringportal-daf0a",
-  storageBucket: "hiringportal-daf0a.appspot.com",
-  messagingSenderId: "368111516990",
-  appId: "1:368111516990:web:a4d4e5bf0aa82bc456806f",
-  measurementId: "G-XZSDT619BG",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
